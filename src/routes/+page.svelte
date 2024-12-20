e65d1435a264e52ac691b837083ed8671ffd6816
Adrià Blancafort <adriablancafort@gmail.com>
2024-12-20 03:09:46 +0100

    add cabinet grotesk font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button/index.js";
 </script>
+
+<img src="https://cdn.asilor.com/asilor-logo-black.svg" alt="Asilor Logo" width="129" height="33">
  
+<h1 class="font-bold text-6xl font-cabinet-grotesk">Realtime Product Price Tracker</h1>
+
 <Button>Click me</Button>
