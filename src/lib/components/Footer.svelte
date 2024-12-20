c6c8302d2e6cc439aa227cba55a3acb0215c794c
Adrià Blancafort <adriablancafort@gmail.com>
2024-12-21 00:29:53 +0100

    add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,9 @@
+<footer class="border-t">
+    <div class="container py-4">
+        <div class="text-sm text-muted-foreground">
+            Copyright
+            <a href="/" class="font-medium underline underline-offset-4">Price Tracker</a>.
+            All rights reserved.
+        </div>
+    </div>
+</footer>
