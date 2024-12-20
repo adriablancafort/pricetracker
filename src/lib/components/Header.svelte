995de5c98cadd08d2a10f50d9846235ffaa65426
Adrià Blancafort <adriablancafort@gmail.com>
2024-12-21 00:35:16 +0100

    correct header dimensions

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,7 @@
 <header class="sticky top-0 z-50 w-full border-b backdrop-blur bg-background/60">
-    <div class="container flex h-14 items-center">
+    <div class="container flex h-16 items-center">
         <a href="/">
-            <img src="/images/price-tracker-logo.svg" alt="Price Tracker Logo" width="250" height="33">
+            <img src="/images/price-tracker-logo.svg" alt="Price Tracker Logo" width="260" height="30">
         </a>
         <nav class="ml-8 flex items-center gap-4 text-sm">
             <a class="text-foreground/80" href="/top-deals">Top Deals</a>
