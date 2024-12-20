eb15557198063d7c19782f4f33dffd8d2efaaada
Adrià Blancafort <adriablancafort@gmail.com>
2024-12-21 00:29:46 +0100

    add header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,11 @@
+<header class="sticky top-0 z-50 w-full border-b backdrop-blur bg-background/60">
+    <div class="container flex h-14 items-center">
+        <a href="/">
+            <img src="/images/price-tracker-logo.svg" alt="Price Tracker Logo" width="250" height="33">
+        </a>
+        <nav class="ml-8 flex items-center gap-4 text-sm">
+            <a class="text-foreground/80" href="/top-deals">Top Deals</a>
+            <a class="text-foreground/80" href="/most-viewed">Most Viewed</a>
+        </nav>
+    </div>
+</header>
